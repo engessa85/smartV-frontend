c89df5e0b48f986644a7e757f3bf3d7120224e39
Mohamed Yehia <eng.essa85@gmail.com>
2025-03-08 20:42:18 +0200

    adjust the error

--- src/components/EmployeeComponents/UserCompanyCard.tsx
+++ src/components/EmployeeComponents/UserCompanyCard.tsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from "react";
+import React, {useState } from "react";
 import Link from "next/link";
 import { MdEmail } from "react-icons/md";
 import { TbWorld } from "react-icons/tb";
